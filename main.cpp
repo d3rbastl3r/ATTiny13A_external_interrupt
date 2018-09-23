fe597bc78e92b3db5fe0674bd1f8799e2c8d32ff
d3rbastl3r <igor.martens.83@gmail.com>
2018-09-23 19:09:41 +0200

    Add implementation of external interrupt

--- main.cpp
+++ main.cpp
@@ -8,14 +8,26 @@
 #define F_CPU 9600000UL
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(INT0_vect) {
+    // Do some stuff on Interrupt
+}
 
 void setup() {
+    MCUCR |=
+        (1<<ISC01) |    // (ISC01 = 1 and ISC00 = 1) The rising edge of INT0 generates an interrupt request.
+        (1<<ISC00);     // (ISC01 = 1 and ISC00 = 1) The rising edge of INT0 generates an interrupt request.
+
+    GIMSK |= (1<<INT0); // Enable external interrupt on PB1
+
+    sei();              // enable all interrupts
 }
 
 int main(void) {
     setup();
 
-    while(1) {         // read value from ADC
+    while(1) {
     }
 
     return 0;
